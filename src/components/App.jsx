83a87254deb4cceb631911690c6f1e6220efa2e8
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-09-05 23:08:11 +0300

    basic rout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import {
+  CONTACTS_ROUTE,
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  REGISTER_ROUTE,
+  appRouts,
+} from './config/routes';
+import NotfoundPage from './pages/NotfoundPage';
+import { Suspense } from 'react';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <header>
+        <nav>
+          <NavLink to={HOME_ROUTE}>Home</NavLink>
+          <NavLink to={CONTACTS_ROUTE}>Contacts</NavLink>
+          <NavLink to={LOGIN_ROUTE}>Login</NavLink>
+          <NavLink to={REGISTER_ROUTE}>Register</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <>
+              {appRouts.map(({ path, element }) => (
+                <Route key={path} path={path} element={element} />
+              ))}
+            </>
+            <>
+              <Route path="*" element={<NotfoundPage />} />
+            </>
+          </Routes>
+        </Suspense>
+      </main>
+    </>
   );
 };
---
+++ src/components/pages/ContactsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactsPage = () => {
+  return (
+    <div>Contacts.Page</div>
+  )
+}
+
+export default ContactsPage
